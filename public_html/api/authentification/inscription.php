4e11abf42b94e033938876a17b332d300b503a70
nahodura <nahodurand@gmail.com>
2025-06-29 11:37:37 -0400

    Ajout jeux par défault + folder img

--- public_html/api/authentification/inscription.php
+++ public_html/api/authentification/inscription.php
@@ -25,6 +25,30 @@ $requete->execute([$nom, $hash]);
 
 $id_utilisateur = $pdo->lastInsertId();
 
+// insére 15 jeux par défaut
+$jeuxParDefault = [
+    ['Zelda', 'Adventure game', 'Adventure', 'Switch', 'zelda.jpg'],
+    ['Minecraft', 'Sandbox building game', 'Sandbox', 'PC', 'minecraft.jpg'],
+    ['Among Us', 'Multiplayer deduction game', 'Party', 'Mobile', 'amongus.jpg'],
+    ['Super Mario Odyssey', '3D platformer', 'Platformer', 'Switch', 'mario.jpg'],
+    ['Stardew Valley', 'Farming simulation', 'Simulation', 'PC', 'stardew.jpg'],
+    ['Overwatch', 'Team shooter', 'FPS', 'PC', 'overwatch.jpg'],
+    ['Fortnite', 'Battle royale', 'Battle Royale', 'PC', 'fortnite.jpg'],
+    ['Celeste', 'Challenging platformer', 'Platformer', 'Switch', 'celeste.jpg'],
+    ['Animal Crossing', 'Life simulation', 'Simulation', 'Switch', 'acnh.jpg'],
+    ['Hades', 'Action roguelike', 'Action', 'Switch', 'hades.jpg'],
+    ['Apex Legends', 'Hero shooter battle royale', 'Battle Royale', 'PC', 'apex.jpg'],
+    ['Halo Infinite', 'Sci-fi shooter', 'FPS', 'Xbox', 'haloinfinite.jpg'],
+    ['The Witcher 3', 'Fantasy RPG', 'RPG', 'PC', 'witcher3.jpg'],
+    ['Portal 2', 'Puzzle platformer', 'Puzzle', 'PC', 'portal2.jpg'],
+    ['Cyberpunk 2077', 'Open world RPG', 'RPG', 'PC', 'cyberpunk.jpg']
+];
+
+$insertJeu = $pdo->prepare("INSERT INTO jeux (nom, description, genre, plateforme, image, id_utilisateur) VALUES (?, ?, ?, ?, ?, ?)");
+foreach ($jeuxParDefault as $game) {
+    $insertJeu->execute(array_merge($game, [$id_utilisateur]));
+}
+
 // nouvelle session
 $_SESSION['id_utilisateur'] = $id_utilisateur;
 $_SESSION['nom_utilisateur'] = $nom;
